798256bf4d24a4a60366d5e8c214790d8dc5c65e
Denis <aripash@gmail.com>
2020-04-13 22:17:01 +0300

    idk

--- test.cpp
+++ test.cpp
@@ -3,14 +3,15 @@
 #include <iostream>
 #include <string>
 
-TEST_CASE("testing the test"){
+/*TEST_CASE("testing the test"){
 	family::Tree T ("test");
 	for(int i = 0; i <101; i++){
 	CHECK(T.relation(to_string(i)).compare("me")==0);	
 	}
-}
+}*/
 
 TEST_CASE("idk"){
 	family::Tree T ("idk");
-	CHECK_THROWS(T.addMother("",""));
+	CHECK(T.relation("idk").compare("unrelated")==0);
+	CHECK(T.relation("id").compare("unrelated")==0);
 }
