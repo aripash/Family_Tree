7ec28f8935bce1c23e721ec085b9a43d8e35e14e
aripash <44765570+aripash@users.noreply.github.com>
2020-04-13 22:19:06 +0300

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include "FamilyTree.hpp"
+#include "doctest.h"
+#include <iostream>
+#include <string>
+
+/*TEST_CASE("testing the test"){
+	family::Tree T ("test");
+	for(int i = 0; i <101; i++){
+	CHECK(T.relation(to_string(i)).compare("me")==0);	
+	}
+}*/
+
+TEST_CASE("idk"){
+	family::Tree T ("idk");
+	CHECK(T.relation("idk").compare("unrelated")==0);
+	CHECK(T.relation("id").compare("unrelated")==0);
+}
