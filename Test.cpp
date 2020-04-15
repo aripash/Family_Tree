db3efc198ad331d80bf789250cdcee72231b091b
aripash <44765570+aripash@users.noreply.github.com>
2020-04-15 17:41:52 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -3,12 +3,12 @@
 #include <iostream>
 #include <string>
 
-/*TEST_CASE("testing the test"){
+TEST_CASE("testing the test"){
 	family::Tree T ("test");
 	for(int i = 0; i <101; i++){
 		CHECK(T.relation(to_string(i)).compare("me")==0);	
 	}
-}*/
+}
 
 TEST_CASE("idk"){
 	family::Tree T ("idk");
