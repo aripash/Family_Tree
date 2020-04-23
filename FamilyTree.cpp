a09254a63991bbba70751c6b5b60f07c7c6a18f2
Denis <aripash@gmail.com>
2020-04-23 12:05:10 +0300

    implemented some of cpp

--- FamilyTree.cpp
+++ FamilyTree.cpp
@@ -1,26 +1,88 @@
 #include "FamilyTree.hpp"
 #include <iostream>
+#include <stdexcept>
+#include <queue> 
 using namespace std;
 using namespace family;
 
-Tree :: Tree(string nroot){
+int depthlength(int depth){
+	int out=0;
+	while(depth!=0){
+		depth/=10;
+		out++;
+	}
+	return out;
+}
+
+Tree :: Tree(string name){
+	_name=name;
+	_m=NULL;
+	_f=NULL;
+	_depth=0;
+}
+Tree :: Tree(string name,int depth){
+	_name=name;
+	_m=NULL;
+	_f=NULL;
+	_depth=depth;
 }
 Tree& Tree :: addFather(string to,string add){
+	Tree* temp=search(to);
+	temp->_f=new Tree(add,temp->_depth*10+2);
 	return *this;
 }
 Tree& Tree :: addMother(string to,string add){
+	Tree* temp=search(to);
+	temp->_m=new Tree(add,temp->_depth*10+1);
 	return *this;
 }
 string Tree :: display(){
-	return "";
+	string out=_name+"\n";
+	queue <Tree*> bst;
+	bst.push(_m);
+	bst.push(_f);
+	int depth=1;
+	while(!bst.empty()){
+		Tree* temp=bst.front();
+		bst.pop();
+		if(depthlength(temp->_depth)==depth)out+=temp->_name+" ";
+		else {
+			out+="\n"+temp->_name+" ";
+			depth++;
+		}
+		bst.push(temp->_m);
+		bst.push(temp->_f);
+	}
+	return out;
 }
-string Tree :: relation(string which){
-	return "me";
+string Tree :: relation(string who){
+	int d=search(who)->_depth;
+	if(d==0)return "me";
+	string out="";
+	if(d%10==1)out+="mother";
+	else out+="father";
+	for(int i=0;i<depthlength(d)-1;i++){
+		out="grand-"+out;
+	}
+	return out;
+	
 }
-string Tree :: find(string who){
+string Tree :: find(string which){
+	if(which.compare("me")==0)return _name;
+	if(which.compare("father")==0)return _f->_name;
+	if(which.compare("mother")==0)return _m->_name;
 	return "";
 }
 void Tree :: remove(string who){
+	if(who.compare(_name))throw invalid_argument("cant delete root");
 }
-
-
+Tree* Tree :: search(string who){
+	if(who.compare(_name)==0)return this;
+	if(_m!=NULL)return _m->search(who);
+	if(_f==NULL)return _f->search(who);
+	return NULL;
+}
+void Tree :: change(string name, int depth){
+	_name=name;
+	_depth=depth;
+}
--- FamilyTree.hpp
+++ FamilyTree.hpp
@@ -1,11 +1,26 @@
 #include <iostream>
 using namespace std;
 
+
+
+
 namespace family{
 	
 	class Tree{
 		public : 
-		Tree(string nroot);
+		string _name;
+		Tree* _m;
+		Tree* _f;
+		int _depth;
+		Tree(string name);
+		Tree(string name,int depth);
+		Tree(){
+			_name="";
+			_m=nullptr;
+			_f=nullptr;
+		}
+		Tree* search(string who);
+		void change(string name,int depth);
 		Tree& addFather(string to,string add);
 		Tree& addMother(string to,string add);
 		string display();
